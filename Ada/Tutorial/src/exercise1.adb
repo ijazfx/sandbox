b3b86638467a045e8ed4192f0b55143305ff0767
Farrukh Ijaz <ijazfx@gmail.com>
2013-03-29 14:12:30 +0300

    Day 1

---
+++ Ada/Tutorial/src/exercise1.adb
@@ -0,0 +1,6 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Exercise1 is
+begin
+   Put("Farrukh Ijaz");
+end Exercise1;
---
+++ Ada/Tutorial/src/exercise2.adb
@@ -0,0 +1,10 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Exercise2 is
+begin
+   Put("Name: Farrukh Ijaz");
+   New_Line;
+   Put("Address: Faisalabad, Pakistan");
+   New_Line;
+   Put("Phone: 1234567");
+end;
